ff6665cd7aa0c04d0e46ef38380aca04fead4b11
陈双林 <chensl@hadlinks.com>
2017-06-22 10:37:25 +0800

    增加业务逻辑描述的流程图

--- SLChartLibDemo/SLChartLibDemo/ViewController.h
+++ SLChartLibDemo/SLChartLibDemo/ViewController.h
@@ -6,6 +6,18 @@
 //  Copyright © 2017年 Hadlinks. All rights reserved.
 //
 
+/*!  头文件基本信息。这个用在每个源代码文件的头文件的最开头。
+ 
+ @header 这里的信息应该与该源代码文件的名字一致
+ 
+ @abstract 关于这个源代码文件的一些基本描述
+ 
+ @author Sindri Lin (作者信息)
+ 
+ @version 1.00 2012/01/20 Creation (此文档的版本信息)
+ 
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
